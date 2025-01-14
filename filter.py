3ed0ef0e5974fddd1be5fe20971777dbfa4f949e
PayBoa <pay.borhani@gmail.com>
2025-01-14 11:16:21 +0100

    comments

--- filter.py
+++ filter.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
 # Charger l'image
-image = Image.open("logo0.png").convert("RGBA")  # Utilisation du mode RGBA pour les images avec transparence
+image = Image.open("logo0.png").convert("RGBA")  # Utilisation du mode RGBA pour les images avec transparence / fichier PNG uniquement
 pixels = image.load()
 
 # Couleur à modifier (ancienne) et couleur de remplacement (nouvelle)
-ancienne_couleur = (0, 0, 0, 255)  # Rouge pur avec opacité complète (RGBA)
-nouvelle_couleur = (0, 71, 71, 255)  # Vert pur avec opacité complète (RGBA)
+ancienne_couleur = (0, 0, 0, 255)  # REMPLIR
+nouvelle_couleur = (255, 255, 255, 255)  # REMPLIR
 
 # Parcourir tous les pixels et modifier la couleur
 largeur, hauteur = image.size
