7d6206b5e444b2f1bbe4b76cb795f5a2d8401057
Payam Borhani <pay.borhani@gmail.com>
2025-01-16 17:57:47 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,21 +1,17 @@
 from PIL import Image
 
-# Charger l'image
-image = Image.open("logo0.png").convert("RGBA")  # Utilisation du mode RGBA pour les images avec transparence / fichier PNG uniquement
+image = Image.open("logo0.png").convert("RGBA")
 pixels = image.load()
 
-# Couleur à modifier (ancienne) et couleur de remplacement (nouvelle)
 ancienne_couleur = (0, 0, 0, 255)  # REMPLIR
 nouvelle_couleur = (255, 255, 255, 255)  # REMPLIR
 
-# Parcourir tous les pixels et modifier la couleur
 largeur, hauteur = image.size
 for x in range(largeur):
     for y in range(hauteur):
-        if pixels[x, y] == ancienne_couleur:  # Vérifie si le pixel correspond à l'ancienne couleur
-            pixels[x, y] = nouvelle_couleur  # Remplace par la nouvelle couleur
+        if pixels[x, y] == ancienne_couleur: 
+            pixels[x, y] = nouvelle_couleur 
 
-# Sauvegarder l'image modifiée
 image.save("logo1.png")
 print("Image modifiée et sauvegardée!")
 
